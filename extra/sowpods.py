68d91c7f70e9b0704026f5806f958e6c756b2c45
avlharshitha <avlharshitha@gmail.com>
2019-06-10 18:28:16 +0530

    updated function names

--- extra/sowpods.py
+++ extra/sowpods.py
@@ -1,49 +1,34 @@
 import sys
 
-
-def are_anagrams(str1, str2):
-    return sorted(str1) == sorted(str2)
-
-
 word_dict = {}
 
 
-def get_anag_list(word):
-    anag_list = word_dict.get("".join(sorted(word)), [])
-    return anag_list
+def extract_anag_list(word_dict):
+    return [anaglist for anaglist in word_dict.values() if len(anaglist) > 1]
 
 
-def gen_anag_list(word_dict):
-    return [wordlist for wordlist in word_dict.values() if len(wordlist) > 1]
-
-
-def gen_word_dict(wordlist):
+def build_anag_list(wordlist):
     for i in wordlist:
-        anag_list = get_anag_list(i)
-        anag_list.append(i)
-        word_dict["".join(sorted(i))] = anag_list
+        word_dict["".join(sorted(i))] = word_dict.get("".join(sorted(i)), [])
+        word_dict["".join(sorted(i))].append(i)
     return word_dict
 
 
-def parse_file(filename):
-    words = []
-    fp = open(filename, "r")
-    for line in fp:
-        words.append(line.strip("\n"))
-    return words
+def get_all_words(filename):
+    return [line.strip("\n") for line in open(filename, "r")]
 
 
-def write_file(filename, anag_list):
+def print_anagrams(filename, anag_list):
     fp = open(filename, "w")
     for anag in anag_list:
         fp.write("%s\n" %anag)
 
 
 def main(input_file, output_file):
-    word_list = parse_file(input_file)
-    word_dict = gen_word_dict(word_list)
-    anag_list = gen_anag_list(word_dict)
-    write_file(output_file, anag_list)
+    word_list = get_all_words(input_file)
+    word_dict = build_anag_list(word_list)
+    anag_list = extract_anag_list(word_dict)
+    print_anagrams(output_file, anag_list)
 
 
 main(sys.argv[1], sys.argv[2])
