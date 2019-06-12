5bc6b960695d7fd7672d7af065a6431dda038a94
avlharshitha <avlharshitha@gmail.com>
2019-06-12 10:21:04 +0530

    using better built in functions -- one line code

--- extra/interweavezip.py
+++ extra/interweavezip.py
@@ -1,12 +1,9 @@
-import functools
+import itertools
 import sys
 
 
 def inter_zip_string(str1, str2):
-    fin_list = list(zip(str1, str2))
-    fin_str = functools.reduce(lambda x, y: "".join(list(x) + list(y)), fin_list)
-    min_len = min(len(str1), len(str2))
-    fin_str += str1[min_len:] + str2[min_len:]
-    return fin_str
+    return "".join(["".join(pair) for pair in list(itertools.zip_longest(str1, str2, fillvalue=""))])
+
 
 print(inter_zip_string(sys.argv[1], sys.argv[2]))
